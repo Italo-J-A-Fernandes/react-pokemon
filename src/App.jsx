df8d6be6840b5f20fa4a85dec0fd5fa1a3a720af
Italo José Araujo Fernandes <italojafernandes@hotmail.com>
2020-09-21 15:13:56 -0300

    Config do ESLint, EditorConfig e Prettier

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Route, Switch } from 'react-router-dom';
 import MapPage from './screens/Map/MapPage';
 
 const App = () => (
-    <Switch>
-        <Route path="/map" component={MapPage} />
-    </Switch>
+  <Switch>
+    <Route path="/map" component={MapPage} />
+  </Switch>
 );
 
 export default App;
